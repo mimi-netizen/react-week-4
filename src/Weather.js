b4bfa30ffd0d0d1d52bc7c0b9e06505c3686af40
Celyne <dianakydd2@gmail.com>
2024-01-12 12:24:47 +0300

    created react app and added axios

---
+++ src/Weather.js
@@ -0,0 +1,19 @@
+import React from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
+    );
+  }
+
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(handleResponse);
+
+  return <h3>Hello</h3>;
+}
